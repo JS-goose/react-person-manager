3a2fc0ef64cb7594d97170972423f9fbbe8f367e
Jonathan Sexton <joncsexton@gmail.com>
2019-04-04 04:48:28 -0500

    Added render for props.children + comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Testing React Lawl</h1>
-        <Person name={'Jonathan'} age={33}/>
-        <Person name={'Stephanie'} age={29}/>
-        <Person name={'Travis'} age={38}/>
+        <Person name={'Jonathan'} age={33} />
+        <Person name={'Stephanie'} age={29} />
+        <Person name={'Travis'} age={38}>
+          {/*Anything placed between component tags can be accessed via props.children*/}
+          My hobbies: Gaming
+        </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 const person = (props) => {
-    return <p>I'm {props.name} and I am {props.age} years old!</p>
-}
+  return (
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      {/* props.children allows me to render content that is placed between the opening and closing tags of my component in App.js */}
+      <p>{props.children}</p>
+    </div>
+  );
+};
 
-export default person
+export default person;
