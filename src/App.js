d90bc3ddb49a0c3573a83ff476eed0714eb62e3c
Jonathan Sexton <joncsexton@gmail.com>
2019-04-04 04:58:00 -0500

    added state and changed the Person rendering to reflect that

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+     {name: 'Jonathan', age: 33},
+      {name: 'Stephanie', age: 29},
+      {name: 'Travis', age: 38},
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Testing React Lawl</h1>
-        <Person name={'Jonathan'} age={33} />
-        <Person name={'Stephanie'} age={29} />
-        <Person name={'Travis'} age={38}>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
           {/*Anything placed between component tags can be accessed via props.children*/}
           My hobbies: Gaming
         </Person>
