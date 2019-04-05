22c60f4108aaf7691d6421557888b581f7f769d8
Jonathan Sexton <joncsexton@gmail.com>
2019-04-05 12:57:12 -0500

    passed click handler to Person component

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-     {name: 'Jonathan', age: 33},
-      {name: 'Stephanie', age: 29},
-      {name: 'Travis', age: 38},
-    ]
-  }
+      { name: 'Jonathan', age: 33 },
+      { name: 'Stephanie', age: 29 },
+      { name: 'Travis', age: 38 },
+    ],
+  };
 
   nameSwitchHandler = (event) => {
     // console.log(event.target);
     this.setState({
       persons: [
-        {name: 'Bill Jackson', age: 33},
-         {name: 'Stephanie', age: 29},
-         {name: 'Travis Bacon', age: 38},
-       ]
+        { name: 'Bill Jackson', age: 33 },
+        { name: 'Stephanie', age: 29 },
+        { name: 'Travis Bacon', age: 38 },
+      ],
     });
-  }
+  };
 
   render() {
     return (
@@ -29,7 +29,11 @@ class App extends Component {
         <button onClick={this.nameSwitchHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+        <Person
+          nameSwitchHandler={this.nameSwitchHandler}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
           {/*Anything placed between component tags can be accessed via props.children*/}
           My hobbies: Gaming
         </Person>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const person = (props) => {
   return (
     <div>
-      <p>
+      <p onClick={props.nameSwitchHandler}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       {/* props.children allows me to render content that is placed between the opening and closing tags of my component in App.js */}
