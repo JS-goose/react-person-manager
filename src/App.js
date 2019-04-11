0133451a221dfb241f426dc6ba17aca9e8b96b25
Jonathan Sexton <joncsexton@.com>
2019-04-11 18:30:34 -0500

    code format change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Jonathan', age: 33 },
-      { name: 'Stephanie', age: 29 },
-      { name: 'Travis', age: 38 },
+      { name: "Jonathan", age: 33 },
+      { name: "Stephanie", age: 29 },
+      { name: "Travis", age: 38 },
     ],
   };
 
@@ -21,15 +21,29 @@ class App extends Component {
     });
   };
 
-  render() {
+  handleInputName = (event) => {
+    console.log(event.target);
+  };
+
+  render() {  
     return (
       <div className="App">
         <h1>React Course :)</h1>
-        <button style = {style} onClick={()=>this.nameSwitchHandler('Johnny Boy!', 'Stephy', 'Travvy')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.nameSwitchHandler("Johnny Boy!", "Stephy", "Travvy")}
+        >
+          Switch Name
+        </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person
-          nameSwitchHandler={this.nameSwitchHandler.bind(this, '**JONATHAN**', '***STEPHANIE***', '<---TRAVIS--->')}
+          nameSwitchHandler={this.nameSwitchHandler.bind(
+            this,
+            "**JONATHAN**",
+            "***STEPHANIE***",
+            "<---TRAVIS--->"
+          )}
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
         >
@@ -42,13 +56,13 @@ class App extends Component {
 }
 
 const style = {
-  padding: '5px',
-  borderRadius: '3px',
-  border: 'none',
-  boxShadow: '1px 1px 3px lightblue',
-  color: 'white',
-  backgroundColor: 'gray',
-  cursor: 'pointer',
-}
+  padding: "5px",
+  borderRadius: "3px",
+  border: "none",
+  boxShadow: "1px 1px 3px lightblue",
+  color: "white",
+  backgroundColor: "gray",
+  cursor: "pointer",
+};
 
 export default App;
