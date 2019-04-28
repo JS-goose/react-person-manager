1303d4819f039c8d6b96387e7253c67855d32636
Jonathan Sexton <joncsexton@gmail.com>
2019-04-28 17:11:57 -0500

    moved functionality to button instead of person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,13 +4,13 @@ import './Person.css';
 const person = (props) => {
   return (
     <div className="person-container">
-      <p onClick={props.click}>
+      <p>
         I'm {props.name} and I am {props.age} years old!
       </p>
       {/* props.children allows me to render content that is placed between the opening and closing tags of my component in App.js */}
       <p>{props.children}</p>
       <input type="text" onChange={props.handleInputName} placeholder={`i.e. ${props.name}`} />
-      <button>Delete</button>
+      <button onClick={props.click}>Delete Person</button>
     </div>
   );
 };
