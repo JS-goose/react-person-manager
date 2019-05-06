a2265982517d380058867abc4cc750b16d7b67db
Jonathan Sexton <joncsexton@gmail.com>
2019-05-06 05:41:46 -0500

    exported error boundary

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
-    state = {
-        hasError: false,
-        errorMessage: '',
-    }
+  state = {
+    hasError: false,
+    errorMessage: '',
+  };
 
-    componentDidCatch = (error, info) => {
-        this.setState({hasError: true, errorMessage: error});
+  componentDidCatch = (error, info) => {
+    this.setState({ hasError: true, errorMessage: error });
+  };
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong: {this.state.errorMessage}</h1>;
+    } else {
+        return this.props.children;
     }
-    render() {
-        return(
-            <h1>Something went wrong</h1>
-        )
-    }
+  }
 }
+
+export default ErrorBoundary
