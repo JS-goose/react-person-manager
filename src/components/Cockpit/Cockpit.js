e4664c959e0425f25e7be82779e78f0c0d0822d2
Jonathan Sexton <joncsexton@gmail.com>
2019-05-09 06:18:37 -0500

    updated click methods for buttons

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,10 +5,10 @@ const cockpit = (props) => {
   return (
     <div>
       <h1 className={assignedClasses.join(' ')}>React Course :)</h1>
-      <button className={classes.buttonStyle} onClick={this.togglePersonsDiv} key={1}>
+      <button className={classes.buttonStyle} onClick={props.togglePersonsDiv} key={1}>
         Show/Hide Persons
       </button>
-      <button className={classes.buttonStyle} onClick={this.restorePersons} key={2}>
+      <button className={classes.buttonStyle} onClick={props.restorePersons} key={2}>
         Restore Persons
       </button>
     </div>
