ca5902901bed59d098f3c3735702dd829b1873da
Jonathan Sexton <joncsexton@gmail.com>
2019-05-13 05:12:31 -0500

    changed lifecycle method

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     users: [],
   };
 
-  componentDidMount = async () => {
+  componentWillMount = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
     this.setState({users: data});
