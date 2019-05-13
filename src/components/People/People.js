fc835a11534cef993f2de36223a9fc01a26048ed
Jonathan Sexton <joncsexton@gmail.com>
2019-05-13 04:53:49 -0500

    Added update prop change lifecycle methods for practice

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log(`[People.js] getDerivedStateFromProps`);
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(`[People.js] shouldComponentUpdate`);
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log(`[People.js] getSnapshotBeforeUpdate`);
+    return { message: 'Snapshot' };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(`[People.js] componentDidUpdate`);
+    console.log(snapshot);
+  }
+
   render() {
+    console.log(`[People.js] rendering...`);
     return this.props.persons.map((person, index) => {
       return (
         <Person
