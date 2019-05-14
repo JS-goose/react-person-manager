3abdfcb348d1da0752cd9a560f065a00a48df09e
Jonathan Sexton <joncsexton@gmail.com>
2019-05-14 04:35:42 -0500

    added additinal hook to see effect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -10,13 +10,20 @@ const cockpit = (props) => {
     setTimeout(() => {
       alert('Saved data to cloud!');
     }, 1000);
-    // This returned function will run before the main useEffect function but after the first render cycle  
-    return() => {
-      console.log(`[Cockpit.js] cleanup in useEffect..`)
-    }
+    // This returned function will run before the main useEffect function but after the first render cycle
+    return () => {
+      console.log(`[Cockpit.js] cleanup in useEffect..`);
+    };
     //If I want this to run on the first render ONLY, I  would pass an empty array here.  If I want it to run
-    // once something updates, I pass in those values to the array.
+    // once something updates, I pass in those values to the array and this will run only when those are updated.
   }, []);
+
+  useEffect(() => {
+    console.log(`[Cockpit.js] 2nd useEffect`);
+    return () => {
+      console.log(`[Cockpit.js] cleanup in 2nd useEffect..`);
+    };
+  });
 
   const assignedClasses = [];
   if (props.persons.length < 1) {
