13eddd9aabcd8117fa77db92d61b7737a5958030
Jonathan Sexton <joncsexton@.com>
2019-05-17 11:08:30 -0500

    added a login button

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -15,22 +15,25 @@ class Person extends Component {
 
   render() {
     return (
-      <div className={classes.personContainer}>
-        <p>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        {/* props.children allows me to render content that is placed between the opening and closing tags of my component in App.js */}
-        <p>{this.props.children}</p>
-        <input
-          ref={this.myInputRef}
-          type="text"
-          onChange={this.props.handleInputName}
-          placeholder={`i.e. ${this.props.name}`}
-        />
-        <button style={buttonStyle} onClick={this.props.deletePerson}>
-          Delete Person
-        </button>
-      </div>
+      <React.Fragment>
+        <div className={classes.personContainer}>
+          <p>
+            I'm {this.props.name} and I am {this.props.age} years old!
+          </p>
+          {/* props.children allows me to render content that is placed between the opening and closing tags of my component in App.js */}
+          <p>{this.props.children}</p>
+          <input
+            ref={this.myInputRef}
+            type="text"
+            onChange={this.props.handleInputName}
+            placeholder={`i.e. ${this.props.name}`}
+          />
+          <button style={buttonStyle} onClick={this.props.deletePerson}>
+            Delete Person
+          </button>
+          <button style = {buttonStyle} login={this.props.login}>Log in</button>
+        </div>
+      </React.Fragment>
     );
   }
 }
