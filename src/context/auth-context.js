49ce7cc5bae99f01325c8a9df9c64e4feb31f82a
Jonathan Sexton <joncsexton@gmail.com>
2019-05-21 05:17:20 -0500

    added comment about default value

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,6 +1,7 @@
 // This is a globally available object but could also be an array, string, number, etc. and can be passed between components without using props
 import React from 'react';
 
+// This takes a default value that will apply if/when I don't set a value
 const authContext = React.createContext({
     isLoggedIn: false,
     login: () => {},
